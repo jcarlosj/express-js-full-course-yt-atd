72fda92bb012b4e6478defd1e31366c844d63586
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2025-04-06 12:05:44 -0500

    [+] index.mjs: Route Parameters

--- src/index.mjs
+++ src/index.mjs
@@ -1,18 +1,31 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const mockUsers = [
+    { id: 1, username: 'manu', displayName: 'Manuela Gomez' },
+    { id: 2, username: 'juli', displayName: 'Juliana Puerta' },
+    { id: 3, username: 'caro', displayName: 'Carolina Rojas' }
+];
 
 /** Middleware: EndPoints */
 app.get( '/', ( req = Request, res = Response ) => {
     res.status( 201 ).send( { msg: "Hello, World!" } );
 } );
 app.get( '/api/users', ( req = Request, res = Response ) => {
-    res.send( [
-        { id: 1, username: 'manu', displayName: 'Manuela Gomez' },
-        { id: 2, username: 'juli', displayName: 'Juliana Puerta' },
-        { id: 3, username: 'caro', displayName: 'Carolina Rojas' }
-    ] );
+    res.send( mockUsers );
+} );
+app.get( '/api/users/:id', ( req = Request, res = Response ) => {
+    console.log( req.params );
+
+    const parsedId = parseInt( req.params.id );
+    if( isNaN( parsedId ) ) res.status( 400 ).send({ msg: "Bad Request. Invalid Id." });
+
+    const findUser = mockUsers.find( ( user ) => user.id === parsedId );
+    if( ! findUser ) return res.sendStatus(404);
+
+    return res.send( findUser );
 } );
 app.get( '/api/products', ( req = Request, res = Response ) => {
     res.send( [
